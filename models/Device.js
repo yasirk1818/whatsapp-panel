29676a957e34d6cd1f8660831a77f15c12d7530f
yasirk1818 <yasirk1818@gmail.com>
2025-07-21 09:09:03 -0700

    Update Device.js

--- models/Device.js
+++ models/Device.js
@@ -3,7 +3,8 @@ const deviceSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     name: { type: String, required: true },
     session: { type: String },
-    // Feature Toggles
+    phoneNumber: { type: String }, // Yeh field add hui hai
+    // --- Feature Toggles ---
     autoRead: { type: Boolean, default: false },
     alwaysOnline: { type: Boolean, default: false },
     rejectCalls: { type: Boolean, default: false },
